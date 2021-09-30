779871e290c204d2597653a9cae1c5af73ae7839
Afimka <a.vime@yandex.ru>
2021-09-30 16:00:01 +0300

    добавил строку

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!+1+++");
+            Console.WriteLine("Hello World!+1+++ krutilin");
         }
     }
 }
